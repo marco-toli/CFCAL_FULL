71aa9ba879bc4f721c85f3ba21b9711bff24a9d2
marcotoliman <marco.toliman.lucchini@cern.ch>
2013-11-15 12:04:13 +0100

    88mm_config_file_added

--- include/MyMaterials.hh
+++ include/MyMaterials.hh
@@ -28,6 +28,7 @@ public:
   static G4Material* LuAG_Pr();
   static G4Material* PWO();
   static G4Material* DSBCe();
+  static G4Material* SiO2Ce();
   
   static G4double fromNmToEv(G4double wavelength);
   static G4double fromEvToNm(G4double energy);
